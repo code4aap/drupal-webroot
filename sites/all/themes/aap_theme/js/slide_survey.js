1aea06c83051010ae4c602c99d083a8e97b435b4
Sumeet Pareek <sumeet.pareek@innoraft.com>
2013-08-17 11:52:22 +0530

    Corrected survey JS code
    references code4aap/drupal-aap-campaign#117

--- sites/all/themes/aap_theme/js/slide_survey.js
+++ sites/all/themes/aap_theme/js/slide_survey.js
@@ -3,37 +3,35 @@
 			attach: function(context) { 
 				// Proceed only if the page has a .surveymonkey-drupal-popup element
 				if ($('.surveymonkey-drupal-popup')) {
+					// Get the required details
+					var survey_delay = Drupal.settings.aap_tweaks.survey_delay;
+					var survey_in_cookie = $.cookie("aap_survey_tracker");
+					var survey = $('.surveymonkey-drupal-popup').detach();
+					var survey_to_show = survey.attr('id');
+
+					// Creat a link to close the survey, and append it to the survey
+					var close_link = $("<a class='survey-close-btn' href='#'>Close</a>");
+					survey.append(close_link);
+					close_link.click(function(e){
+						$(this).parent().slideUp('slow');
+						// Update the cookie to record what survey was closed, we do not show closed surverys again
+						$.cookie("aap_survey_tracker", survey_to_show);
+						e.preventDefault();
+						return false;
+					});
+
 					// After a set amount of time (we will not make it configurable now) the specific
 					// survery pane is moved from footer to first child of the body and opened with slide effect
-
-					survey_delay = Drupal.settings.aap_tweaks.survey_delay;
-					aap_survey_tracker = $.cookie("aap_survey_tracker");
-					survey = $('.surveymonkey-drupal-popup').detach();
-					survey_id = $(survey).attr('id');
-					
-					// TODO: Do all of this based on cookie aap_survey_tracker
-					// Hint1: Refer this for working with cookie - https://github.com/code4aap/drupal-webroot/issues/18
-					// Hint2: Use CSS ID of the pane for cookie value
-					if (!(aap_survey_tracker == survey_id)){
-						setTimeout(function(){
-							// TODO: A 'Close' button needs to be added as last element in .surverymonkey-drupal-popup
-							// which should close the survery and set the cookie value to not show it again
-							$('body').scrollTop(0);
-							survey_id = $(survey).attr('id');
-							$(survey) // Replace this selector with one suitable for you
-							.append('<input type="button" value="Close">') // Create the element
-							.click(function(){
-								$.cookie("aap_survey_tracker", survey_id);		// set cookie value that user has closed this survey
-								$('body').children().first().hide();
-							});
-							
+					if (survey_in_cookie != survey_to_show) {
+						setTimeout(function() {
+							window.scrollTo(0,0);
+							// Append survey at the start of the page
 							$('body').children().first().before(survey);
 							survey.slideDown('slow');
-						},survey_delay );
+						}, survey_delay);
 					}
 				}
 			}
 	}	
-
 }
 )(jQuery, Drupal, this, this.document); 
