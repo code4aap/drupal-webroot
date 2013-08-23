86b74a231ee05f059044eea5cd3ccda32fdf4c7f
Kuldeep Singh Pal <tkuldeep.singh@innoraft.com>
2013-08-23 21:42:12 +0530

    Added in border in constituency and candidate page
    reference -> code4aap/drupal-aap-campaign#128

--- sites/all/themes/aap_theme/js/constituency-page.js
+++ sites/all/themes/aap_theme/js/constituency-page.js
@@ -4,6 +4,9 @@
 				$(".shs-hierarchy li:nth-child(1)").addClass("state");
 				$(".shs-hierarchy li:nth-child(2)").addClass("district");
 				$(".shs-hierarchy li:nth-child(3)").addClass("constituency");
+				if ( $('.aap-delhi-region .field__item').children().length > 0 ) {
+					 $('.aap-delhi-region .field__item').addClass("aap-delhi-region-content");
+				}
 			}
 	}	
 
